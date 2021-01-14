8582db212edf79d957432f44f624f1c7ec2309ef
arn <arnaud.desaivres@fr.clara.net>
2021-01-14 15:20:42 +0100

    close colorsé

--- deploy.sh
+++ deploy.sh
@@ -28,57 +28,57 @@ SYSTEMYAML="system.yaml"
 
 if [ ! -f $AGENT ] 
 then
-  echo -e "\033[0;31m Getting SmartAgent"
+  echo -e "\033[0;31m Getting SmartAgent\033[0m"
   wget https://github.com/signalfx/signalfx-agent/releases/download/v5.7.1/signalfx-agent-5.7.1.tar.gz
 fi
 if [ ! -f $INITSCRIPT ] 
 then
-  echo -e "\033[0;31m Getting INITSCRIPT"
+  echo -e "\033[0;31m Getting INITSCRIPT\033[0m"
   wget https://raw.githubusercontent.com/arnnow/squeeze-signalfx-agent/master/signalfx-agent
 fi
 if [ ! -f $CONFIG ] 
 then
-  echo -e "\033[0;31m Getting CONFIG"
+  echo -e "\033[0;31m Getting CONFIG\033[0m"
   wget https://raw.githubusercontent.com/arnnow/squeeze-signalfx-agent/master/agent.yaml
 fi
 if [ ! -f $SYSTEMYAML ] 
 then
-  echo -e "\033[0;31m Getting SYSTEMYAML"
+  echo -e "\033[0;31m Getting SYSTEMYAML\033[0m"
   wget https://raw.githubusercontent.com/arnnow/squeeze-signalfx-agent/master/system.yaml
 fi
 
-echo -e "\033[0;31m Setting variable in config file"
+echo -e "\033[0;31m Setting variable in config file\033[0m"
 sed -i "s/TOKEN/${token}/g" $CONFIG
 sed -i "s/ENVIRONMENT/${env}/g" $CONFIG
 sed -i "s/SFXMONITORED/${sfx_monitored}/g" $CONFIG
 
-echo -e "\033[0;31m Uploading to target ${target_host}"
+echo -e "\033[0;31m Uploading to target ${target_host}\033[0m"
 scp signalfx-agent-5.7.1.tar.gz $target_host:/tmp/
 
-echo -e "\033[0;31m Unpacking smartagent on target host"
+echo -e "\033[0;31m Unpacking smartagent on target host\033[0m"
 ssh $target_host "tar -xzf /tmp/signalfx-agent-5.7.1.tar.gz --directory /opt"
 
-echo -e "\033[0;31m Copying init script to ${target_host}"
+echo -e "\033[0;31m Copying init script to ${target_host}\033[0m"
 scp $INITSCRIPT $target_host:/etc/init.d/
 ssh $target_host "chmod +x /etc/init.d/signalfx-agent"
 
-echo -e "\033[0;31m creating signalfx user on target"
+echo -e "\033[0;31m creating signalfx user on target\033[0m"
 ssh $target_host "useradd signalfx -d /opt/signalfx-agent -s /bin/false"
 
-echo -e "\033[0;31m Setting /opt/signalfx-agent permission"
+echo -e "\033[0;31m Setting /opt/signalfx-agent permission\033[0m"
 ssh $target_host "chown -R signalfx:signalfx /opt/signalfx-agent"
 
-echo -e "\033[0;31m Creating confg dir"
+echo -e "\033[0;31m Creating confg dir\033[0m"
 ssh $target_host "mkdir -p /etc/signalfx/conf.d"
 
-echo -e "\033[0;31m Copying config files"
+echo -e "\033[0;31m Copying config files\033[0m"
 scp $CONFIG $target_host:/etc/signalfx
 scp $SYSTEMYAML $target_host:/etc/signalfx/conf.d/
 
-echo -e "\033[0;31m Enabling auto start"
+echo -e "\033[0;31m Enabling auto start\033[0m"
 ssh $target_host "update-rc.d signalfx-agent start"
 
-echo -e "\033[0;31m Cleaning Conf File locally"
+echo -e "\033[0;31m Cleaning Conf File locally\033[0m"
 rm $CONFIG
 rm $SYSTEMYAML  
 rm $INITSCRIPT   
